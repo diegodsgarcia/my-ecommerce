57280126d2f777aeb333ec7722556517950c5c3d
Diego <diego.ds.garcia@gmail.com>
2020-02-05 22:51:52 -0300

    test: add tests in utils

--- src/service/utils.spec.js
+++ src/service/utils.spec.js
@@ -1,4 +1,11 @@
-import { truncate, currency, convertDecimal } from './utils'
+import {
+  truncate,
+  currency,
+  convertDecimal,
+  isValidCreditCardNumber,
+  isValidDateExpiry,
+  isValidCPF,
+} from './utils'
 
 describe('Utils tests:', () => {
   test('Expect text is truncate', () => {
@@ -14,5 +21,29 @@ describe('Utils tests:', () => {
   test('Expect value to be converted in DECIMAL', () => {
     const value = 11025
     expect(convertDecimal(value)).toBe(110.25)
+  })
+
+  test('Expect card number is valid', () => {
+    expect(isValidCreditCardNumber('5338 9860 8756 9325')).toBeTruthy()
+  })
+
+  test('Expect card number is invalid', () => {
+    expect(isValidCreditCardNumber('1111 1111 1111 1111')).toBeFalsy()
+  })
+
+  test('Expect card expirity date is valid', () => {
+    expect(isValidDateExpiry('09/22')).toBeTruthy()
+  })
+
+  test('Expect card expirity date is invalid', () => {
+    expect(isValidDateExpiry('99/99')).toBeFalsy()
+  })
+
+  test('Expect CPF is valid', () => {
+    expect(isValidCPF('769.905.050-95')).toBeTruthy()
+  })
+
+  test('Expect CPF is invalid', () => {
+    expect(isValidCPF('769.905.050-91')).toBeFalsy()
   })
 })
